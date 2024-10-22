d61eaa4e627a87b9c0c056a7878e3eb7fa77f0c5
Zachary Evans <zmevans2017@gmail.com>
2024-10-22 15:43:34 -0600

    Created a AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
+import model.AuthData;
 
 public interface AuthDAO {
+    int createAuthData();
+    void removeAuthData() throws DataAccessException;
+    AuthData findAuthData(int authToken) throws DataAccessException;
+    void removeAuthDatas() throws DataAccessException;
 }
